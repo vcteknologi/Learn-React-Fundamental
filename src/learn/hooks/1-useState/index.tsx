31562866cb6a052f8316cb71057e9b3bef96121f
Wahyu Agus Arifin <68760662+ItPohgero@users.noreply.github.com>
2024-09-12 13:26:31 +0700

    use state

--- src/learn/hooks/1-useState/index.tsx
+++ src/learn/hooks/1-useState/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 const LearnUseState = () => {
-    const [count, setCount] = React.useState(0)
+    const [count, setCount] = React.useState<number>(0)
     return (
         <div>
             <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
+            <button type='button' onClick={() => setCount(count + 1)}>
+                +1
+            </button>
+            <button type='button' onClick={() => setCount((c) => c + 2)}>
+                +2
+            </button>
+            <button type='button' onClick={() => setCount(0)}>
+                Reset
             </button>
         </div>
     )

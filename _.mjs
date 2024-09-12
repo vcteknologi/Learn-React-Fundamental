2a7dc35a414770c98b49c661a029a4639570a145
Wahyu Agus Arifin <68760662+ItPohgero@users.noreply.github.com>
2024-09-12 08:53:52 +0700

    example

---
+++ _.mjs
@@ -0,0 +1,27 @@
+import { execSync } from "node:child_process";
+import readline from "node:readline";
+
+// Function to run a command and handle errors
+const runCommand = (command) => {
+	try {
+		execSync(command, { stdio: "inherit" });
+	} catch (error) {
+		console.error(`Error running command: ${command}`);
+		process.exit(1);
+	}
+};
+
+// Create readline interface for input
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+// Add changes to staging
+runCommand('git add . -- ":!.env"');
+
+// Prompt for commit message
+rl.question("Enter commit message: ", (msg) => {
+	runCommand(`git commit -m "${msg}"`);
+	rl.close();
+});
